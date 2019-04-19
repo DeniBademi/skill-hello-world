00dcc068f7af0be5c01a72883a8c0d1a2476649e
DeniBademi <48317568+DeniBademi@users.noreply.github.com>
2019-04-19 17:05:01 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,32 +1,21 @@
-# Copyright 2016 Mycroft AI, Inc.
 #
-# This file is part of Mycroft Core.
 #
-# Mycroft Core is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
 #
-# Mycroft Core is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#             S.P.A.S. Team 2019
+#             Skill for Picroft control over dedicated S.P.A.S. Droids by Denis Zahariev 
+#             Made with <3 and Python
 #
-# You should have received a copy of the GNU General Public License
-# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 from adapt.intent import IntentBuilder
 
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 from os.path import dirname
-from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill
-from mycroft.util.log import getLogger
 import requests
 import urllib.request
 import ssl
-__author__ = 'eward'
+__author__ = 'deniBademi'
 
 LOGGER = getLogger(__name__)
 
